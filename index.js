3c2d5db1eafd8a45e8adb2c38dc5f7b988afa650
Hassan kh salar <khsalarhassan@gmail.com>
2024-12-05 01:51:17 +0600

    prog

--- index.js
+++ index.js
@@ -12,12 +12,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//khsalarhassan
-//3AT9G9DgEpY342rx
-
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASSWORD)
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qxh3b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -34,12 +28,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const visaCollection = client.db('visaDB').collection('visa');
+
+    app.get('/visa', async(req, res) => {
+        const cursor = visaCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+    app.post('/visa', async(req, res)=>{
+        const newVisa = req.body;
+        console.log(newVisa);
+        const result = await visaCollection.insertOne(newVisa);
+        res.send(result);
+    })
+    
+    
+    
+    
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
